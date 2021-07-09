4231e1257658adbb1b9549e2db8c18192ff2f67e
tiger3120 <zhek4770@gmail.com>
2021-07-09 23:17:57 +0300

    Commented out the file

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
+// Location
 const location = process.argv[2];
 
+// Get the geocode of location
 geocode(location, (error, { latitude, longitude, location}) => {
 
+    // Validate the location
     if (!location) {
         return console.log('Please, provide a location!');
     }
 
+    // Handle error
     if (error) {
         return console.log(error);
     }
     
+    // Get the forecast by lon, lat
     forecast(latitude, longitude, (error, forecastData) => {
+
+        // Handle error
         if (error) {
             return console.log(error);
         }
         
+        // Log the location and forecast
         console.log(location);
         console.log(forecastData);
     });
 });
-
-// geocode('Boston', (error, data) => {
-//     if (error) {
-//         return console.log(error);
-//     }
-    
-//     forecast(latitude, longitude, (error, forecastData) => {
-//         if (error) {
-//             return console.log(error);
-//         }
-        
-//         console.log(location);
-//         console.log(forecastData);
-//     });
-// });
